ef5200410cbd7385771e1e4c419a7b9fc7abe185
zhaoxudong <zhaoxudong@long.tv>
2015-03-14 00:00:14 +0800

    on dev

--- src/mobile-slider.js
+++ src/mobile-slider.js
@@ -1,171 +1,9 @@
 /*!
- * mobile slider
+ * mobile-slider
  * https://github.com/zxdong262/mobile-slider
  * License: MIT
  */
 
-;(function($){
-	function MS(opts, ob) {
-		var defaults = {
-			speed: 500
-			,timer: 4000
-			,autoSlider: true
-			,hasNav: true
-			,pauseOnHover: true
-			,navLeftTxt: '&lt;'
-			,navRightTxt: '&gt;'
-			,zIndex:20
-			,ease: 'linear'
-			,beforeAction: null
-			,afterAction: null
-		}
-		,th = this
-		,defs = th.defs = $.extend(defaults, opts)
-		,cssSet = {
-			position:'absolute'
-			,left:0
-			,right:0
-			,width:'100%'
-			,height:'100%'
-			,'z-index': defs.zIndex
-		}
-		th.t = ob.show().wrapInner('<div class="paper-slides" />')
-		th.p = th.t.children().css(cssSet)
-		th.ps = th.p.children().addClass('paper-slide').css(cssSet)
-		th.len = th.ps.length
-		th.flag = null
-		th.pause = false
-		th.onAction = false
-		th.currentPage = 0
-
-		//init z-index
-		th.ps.eq(0).css('z-index', defs.zIndex + 1).end().filter(':odd').addClass('ps-odd')
-		
-		//auto start
-		if(th.defs.autoSlider) {
-			th.flag = setTimeout(function() {
-				th.autoroll()
-			}, defs.timer)
-		}
-		
-		//OnHover
-		th.t.hover(function() {
-			$(this).addClass('ps-hover')
-			if(defs.pauseOnHover) th.pause = true
-		},function() {
-			$(this).removeClass('ps-hover')
-			if(defs.pauseOnHover) th.pause = false
-		})
-		
-		//paper link
-		th.t.on('click', '.ps-link', function() {
-			if(th.onAction) return
-			th.onAction = true
-			var i1 = parseInt($(this).data('ps-page'))
-			,i2 = (i1 + th.len) % th.len
-			,isNext = i1 > th.currentPage
-			if(i2 === th.currentPage) return
-			th.action(isNext, i2)
-		})
-		
-		//navs
-		if(defs.hasNav) {
-			th.t.append('<a href="javascript:;" class="ps-nav ps-nav-prev">' + defs.navLeftTxt +
-			'</a><a href="javascript:;" class="ps-nav ps-nav-next">' + defs.navRightTxt + '</a>')
-			.children('.ps-nav').css('z-index', defs.zIndex + 10 + th.len)
-			th.t.on('click', '.ps-nav', function() {
-				if(th.onAction) return
-				th.onAction = true
-				var isNext = $(this).hasClass('ps-nav-next')
-				,len = th.len
-				,i = isNext? (th.currentPage + 1 + len) % len : (th.currentPage - 1 + len) % len 
-				th.action(isNext, i)
-			})
-		}
-	}
-	
-	PS.prototype = {
-		action: function(isNext,index) {
-			var th = this
-			,defs = th.defs
-			,speed = defs.speed
-			,c = th.currentPage
-			,ps = th.ps
-			,step = isNext?50 : -50
-			,cp = ps.eq(c)
-			,ip = ps.eq(index)
-			cp.css({
-				'z-index': defs.zIndex + 2
-			}).addClass('ps-on').show()
-			ip.css({
-				'z-index': defs.zIndex + 1
-			}).addClass('ps-on').show()
-			ps.filter(function() {
-				return !$(this).hasClass('ps-on')
-			}).css('z-index', defs.zIndex)
-			$.isFunction(th.defs.beforeAction) && th.defs.beforeAction.call(th)
-			cp.animate({
-				left: -step + '%'
-			}, speed, defs.ease, function() {
-				cp.css('z-index', defs.zIndex + 1).animate({
-					left:0
-				},speed)
-			});
-			ip.animate({
-				left:step
-			}, speed, defs.ease, function() {
-				cp.removeClass('ps-on')
-				ip.css('z-index', defs.zIndex + 2).removeClass('ps-on').animate({
-					left:0
-				}, speed)
-				th.currentPage = index
-				th.onAction = false
-				$.isFunction(th.defs.afterAction) && th.defs.afterAction.call(th)
-				if(defs.autoSlider) {
-					clearTimeout(th.flag)
-					th.flag = setTimeout(function() {
-						th.autoroll()
-					}, defs.timer)
-				}
-			})
-		}
-		,autoroll: function() {
-			var t = this
-			if(!t.onAction && !t.pause) {
-					t.onAction = true
-					var i = (t.currentPage + 1 + t.len) % t.len
-					if(!t.pause) t.action(true,i)
-			}
-			else {
-				clearTimeout(t.flag)
-				t.flag = setTimeout(function() {
-					t.autoroll()
-				}, t.defs.timer)
-			}
-		}
-		,destroy: function() {
-			var t = this
-			clearTimeout(t.flag)
-			t.ps.unwrap()
-			t.t.off( 'click', '**' ).removeAttr('style').children('.ps-nav').remove()
-			t.t.children('.paper-slide').removeAttr('style').removeClass('paper-slide')
-			$.each( t, function( key, value ) {
-				delete t[key]
-			})
-		}
-		
-	}
-	
-	//jquery plugin
-	$.fn.paperSlider = function(opts) {
-		return new PS(opts, this)
-		}
-})(jQuery)
-
-
-/*!
- * ds.js https://github.com/zxdong262/dialog-system
- */
 
 ;(function(window, undefined) {
 
@@ -182,7 +20,9 @@
 
 	//factory
 	function factory($) {
-
+		$.fn.paperSlider = function(opts) {
+			return new PS(opts, this)
+		}
 	}
 
 	function MS(_opts, _$obj) {
@@ -190,7 +30,7 @@
 		defaults = {
 			speed: 1000 //animation time
 			,timer: 4000 //time stops between auto roll
-			,autoSlider: true
+			,autoSlider: false //auto slider
 			,hasNav: true //show nav buttons
 			,navEvent: true //click tap nav button trigger slider
 			,swipeEvent: true //swipe trigger slider
@@ -213,7 +53,7 @@
 		,cssSet = {
 			position:'absolute'
 			,left:0
-			,right:0
+			,top:0
 			,width:'100%'
 			,height:'100%'
 			,'z-index': defs.zIndex
@@ -306,9 +146,53 @@
 		,speed = defs.speed
 		,targetFrame = th.$sliderItems.eq(index)
 		,nextFrame = th.$sliderItems.eq((index + 1 + th.length) % th.length)
+		,anim1 = {}
+		,anim2 = {}
+		,css1 = {}
+		,css2 = {}
+		,prop1 = 'bottom'
+		,prop2 = 'top'
 
-		targetFrame.addClass('ms-prepare-target')
-		nextFrame.addClass('ms-prepare-next')
+		if(defs.vertical && !next) {
+			prop1 = 'top'
+			prop2 = 'bottom'
+		}
+		else if(!defs.vertical && !next) {
+			prop1 = 'left'
+			prop2 = 'right'
+		}
+		else if(!defs.vertical && next) {
+			prop1 = 'right'
+			prop2 = 'left'
+		}
+
+		css1[prop1] = '100%'
+		css1[prop2] = 'auto'
+		anim1[prop1] = 0
+
+		css2[prop1] = '200%'
+		css2[prop2] = 'auto'
+		anim2[prop1] = '100%'
+
+		if($.isFunction(th.defs.beforeAction)) th.defs.beforeAction.call(th)
+		if(defs.vertical) {
+			targetFrame.css(css1)
+			nextFrame.css(css2)
+			targetFrame.animate(anim1, speed)
+			nextFrame.animate(anim2, speed, function() {
+				th.currentPage = index
+				th.onAction = false
+				if($.isFunction(th.defs.afterAction)) th.defs.afterAction.call(th)
+				if(defs.autoSlider) {
+					clearTimeout(th.timerhandler)
+					th.timerhandler = setTimeout(function() {
+						th.autoroll()
+					}, defs.timer)
+				}
+			})
+		}
+
+
 	}
 	MS.prototype.autoroll = function() {
 		var t = this
